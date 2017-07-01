d3ea626f7a218881432661edd6af9d71a1bdea12
songkai <18211182302@163.com>
2017-07-01 14:11:10 +0800

    [optimize] 新建项目 初次上传

---
+++ src/main/java/com/songkai/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.songkai.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+	
+	@RequestMapping(value={"","/"},method=RequestMethod.GET)
+	public String test(){
+		return "index";
+	}
+	
+}
